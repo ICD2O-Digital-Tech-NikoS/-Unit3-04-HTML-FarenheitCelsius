7ec2b807b4cf5296306531416f6d66d3c8e90af8
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-09 20:15:34 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Niko
+// Created on: april 2024
 // This file contains the JS functions for index.html
+
+
+"use strict"
+// function to calculate area
+function calculateCelsius() {
+  // declaring variable for hours worked with value set as value inputed by user 
+  let f = document.getElementById("f").value;
+
+  // calculates result by multiplying the product of side a, side b, and height by 1/6
+  let celsius = (f-32) * (5/9);
+  document.getElementById("user-info").innerHTML = f + "° fahrenheit is equal to " + celsius.toFixed(0) + "°C";
+
+  return celsius;
+}
